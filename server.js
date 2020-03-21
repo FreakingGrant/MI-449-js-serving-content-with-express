49f3abf560a43cfdafba82978993db2b9e54d10a
FreakingGrant <youngsgr@msu.edu>
2020-03-20 20:58:49 -0400

    Reduced redundant code, added Thor character

--- server.js
+++ server.js
@@ -5,13 +5,46 @@ var port = process.env.PORT || 8080
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
+// Create a list of links to pass to the routing values
+links = {}
+
+/**
+ * Adds a Link to the list of Links to be rendered by the browser
+ * @param {string} hrf The href to link to
+ * @param {string} txt The text to be displayed for the link
+ */
+function addLink(hrf, txt) {
+  var id = Object.keys(links).length
+  links[id] = {
+    href: hrf,
+    text: txt
+  }
+}
+
+// Initially add the home links
+addLink('/', 'Home')
+
 // Create a list of characters we will add to the website
 characters = {}
 
-// function that adds a character to the list of characters
+/**
+ * Adds a Character to the list of Characters
+ * @param {Character} character Character object to add to the list of characters
+ */
 function addCharacter (character) {
   var id = Object.keys(characters).length
   characters[id] = character
+  addLink(character.title, character.title)
+
+  var route = '/' + character.title
+  var renderPage = 'pages/' + character.title
+
+  app.get(route, function (request, response) {
+    response.render(renderPage, {
+      character: character,
+      links: links
+    })
+  })
 }
 
 // Create some characters to look at
@@ -24,7 +57,7 @@ var spiderman = {
 }
 
 var ironMan = {
-  title: "Iron Man",
+  title: "Iron-Man",
   image: "/images/iron-man.jpg",
   link: "https://en.wikipedia.org/wiki/Iron_Man_(2008_film)",
   movieReleaseDate: "2008",
@@ -39,53 +72,24 @@ var antMan = {
   description: "Ant-Man is just an awesome and friendly guy. He's wicked smart, super goofy, and overall feels relatable. Plus he was pretty instrumental in helping with the time travel part of things in EndGame. He's consistently funny, and can take down some hardcore villans."
 }
 
+var thor = {
+  title: "Thor",
+  image: "/images/thor.jpg",
+  link: "https://en.wikipedia.org/wiki/Thor_(film)",
+  movieReleaseDate: "2011",
+  description: "I like Thor but honestly this page is just more of a test than anything."
+}
+
 // Let's add them to the list of characters
 addCharacter(spiderman)
 addCharacter(ironMan)
 addCharacter(antMan)
-
-// Create a list of links to pass to the routing values
-links = {}
-
-function addLink(hrf, txt) {
-  var id = Object.keys(links).length
-  links[id] = {
-    href: hrf,
-    text: txt
-  }
-}
-
-// Add some links to the list of links
-addLink('/', 'Home')
-addLink('/spider-man', 'Spider-Man')
-addLink('/ironman', 'Iron-Man')
-addLink('/ant-man', 'Ant-Man')
+addCharacter(thor)
 
 // Route the server requests
 app.get('/', function (request, response) {
   response.render('pages/index', {
     characters: characters,
-    links: links
-  })
-})
-
-app.get('/spider-man', function (request, response) {
-  response.render('pages/spiderman', {
-    character: spiderman,
-    links: links
-  })
-})
-
-app.get('/ironman', function (request, response) {
-  response.render('pages/ironman', {
-    character: ironMan,
-    links: links
-  })
-})
-
-app.get('/ant-man', function (request, response) {
-  response.render('pages/antman', {
-    character: antMan,
     links: links
   })
 })
